6f8a3a97962fd86cd73f03e910d3c89ce1506c79
Tam Bui <tam.bui@spacestep.ca>
2020-01-16 17:44:56 +0700

    add return type to main

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import AppModule from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<string>('application.port');
