3170438879995699bb5bc13a74cf5beee50da34a
Tam Bui <tam.bui@spacestep.ca>
2020-01-18 17:33:42 +0700

    update import

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 /* eslint-disable jest/expect-expect */
 import { Test, TestingModule } from '@nestjs/testing';
-import * as request from 'supertest';
+import request from 'supertest';
 import AppModule from '../src/app.module';
 
 describe('AppController (e2e)', () => {
